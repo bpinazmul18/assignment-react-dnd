5aeed1ce63c4c86ffc40e03a4635e434aaf3ff53
Nazmul Haque <bpinazmul18@gmail.com>
2023-01-28 11:43:02 +0600

    FEAT: Add react-dnd and react-html-backend.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+
+import { DndProvider } from 'react-dnd'
+import { HTML5Backend } from 'react-dnd-html5-backend'
+
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <App />
+    <DndProvider backend={HTML5Backend}>
+      <App />
+    </DndProvider>
   </React.StrictMode>
 )
 
