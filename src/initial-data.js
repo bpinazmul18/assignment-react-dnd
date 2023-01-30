8cf7e42a84804177b2e9ed3d4bda3a32332ce45c
Nazmul Haque <bpinazmul18@gmail.com>
2023-01-30 21:11:55 +0600

    CLEAN: clean up the app.

--- src/initial-data.js
+++ src/initial-data.js
@@ -1,12 +1,6 @@
 const initialData = {
   layout: [],
-  components: {
-    component0: { id: 'component0', type: 'input', content: 'Some input' },
-    component1: { id: 'component1', type: 'image', content: 'Some image' },
-    component2: { id: 'component2', type: 'email', content: 'Some email' },
-    component3: { id: 'component3', type: 'name', content: 'Some name' },
-    component4: { id: 'component4', type: 'phone', content: 'Some phone' },
-  },
+  components: {},
 }
 
 export default initialData
